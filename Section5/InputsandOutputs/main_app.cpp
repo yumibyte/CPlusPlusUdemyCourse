962f51fbf170e84aee794fa4409f0613b9ec91e4
Yumibyte <ashraigosa@gmail.com>
2020-03-29 13:59:46 -0700

    Practice taking inputs and outputs with doubles and integers

--- Section5/InputsandOutputs/main_app.cpp
+++ Section5/InputsandOutputs/main_app.cpp
@@ -7,12 +7,23 @@ int main() {
 	int num1;
 	int num2;
 	double num3;
+//	
+//	cout << "Enter first integer ";
+//	cin >> num1;
+//	
+//	cout << "Enter second integer ";
+//	cin >> num2;
+//	
+//	cout << "You entered " << num1 << " and " << num2 << endl;
+
+	// 2 extraction operators in one line
+//	cout << "Enter 2 integers separated with a space: ";
+//	cin >> num1 >> num2;
+//	cout << "You entered " << num1 << " and " << "num2" << endl;
 	
-	cout << "Enter first integer ";
-	cin >> num1;
+	cout << "Enter a double ";
+	cin >> num3;
 	
-	cout << "Enter second integer ";
-	cin >> num2;
+	cout << "You entered " << num3 << endl;
 	
-	cout << "You entered " << num1 << " and " << num2 << endl;
 }
