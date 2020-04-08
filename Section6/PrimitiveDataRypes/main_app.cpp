289fa2a4b8cca98ba47a6e6856e022bfc81777ed
Yumibyte <ashraigosa@gmail.com>
2020-04-07 21:11:21 -0700

    primitive data types and examples

---
+++ Section6/PrimitiveDataRypes/main_app.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	char middle_initial('Y'); // Single quotes
+	cout << "my mid initial is " << middle_initial << endl;
+	
+	unsigned short int exam_score(55); // unsigned short exam_score
+	cout << "exam score is " << exam_score << endl;
+	
+	int countries_represented (65); 
+	cout << "there were " << countries_represented << " countries" << endl;
+	
+	long people_in_florida (20610000);
+	cout << "there are about " << people_in_florida << " people in Florida" << endl;
+	
+	long people_in_earth = 7600000000; //
+	cout << "there are about " << people_in_earth << " people in Earth" << endl;
+	
+}	
---
+++ Section6/build-Debug/PrimitiveDataRypes/main_app.cpp.o.d
@@ -0,0 +1,1 @@
+../build-Debug/PrimitiveDataRypes/main_app.cpp.o: main_app.cpp
